d6d4f52ea087d1dbef0ae3746a883fe17e7ff073
eduarda12345 <57159917+eduarda12345@users.noreply.github.com>
2022-07-19 16:38:23 -0300

    finalizado

---
+++ SiteprismaCucumber/features/step_definitions/logarusuarioAdmin.rb
@@ -0,0 +1,12 @@
+Dado('Estar na pagina do login')do
+    @logarUsuarioAdmin_page ???
+    @LogarUsuarioAdmin_load.load
+end
+
+Quando('O email e a senha do usuario estiver cadastrada no ecommerce {string} para logar no ecommere ')do |string|
+@logarUsuario = logarUsuario.new
+@logarUsuario.email.set @logarUsuario.generate.email
+@logarUsuario.password.set string
+find('button[type="submit"]').click
+find('button[type = "checkbox"]')
+end
